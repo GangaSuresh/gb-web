2753f6b0d1692f768b9d668c53459257a15d6a6d
Ganga Nair <45668012+GangaSuresh@users.noreply.github.com>
2025-09-05 19:55:46 +0530

    placeholder done

--- src/sections/coin/transaction/index.tsx
+++ src/sections/coin/transaction/index.tsx
@@ -1,3 +1,6 @@
+import { Box, Typography } from "@mui/material";
+import { TYPOGRAPHY } from "src/theme/styles/fonts";
+
 interface TransactionProps {
   isMobile: boolean;
   isTablet: boolean;
@@ -5,6 +8,29 @@ interface TransactionProps {
 
 export default function Transaction({ isMobile, isTablet }: TransactionProps) {
     return (
-        <p>transaction - Mobile: {isMobile ? 'Yes' : 'No'}, Tablet: {isTablet ? 'Yes' : 'No'}</p>
+        <Box sx={{ 
+          display:'flex',
+          justifyContent:'center',
+          background: 'radial-gradient(ellipse at 0% 264%, #D70000 12.86%, #004BE3 100%)',
+        }}>
+          <Box sx={{
+            width:'90%',
+            maxWidth:'1140px',
+            display:'flex',
+            flexDirection:'column',
+            alignItems:'center',
+            justifyContent:'center',
+            color:'white',
+            p: isMobile?'2rem':'3rem',
+            textAlign:'center',
+            border:'solid'
+            }}>
+          <Typography
+          sx={{...(isMobile?TYPOGRAPHY.headline5:TYPOGRAPHY.headline4)}}
+          >UJ Coins History</Typography>
+
+          </Box>
+          
+        </Box>
     );
 }
